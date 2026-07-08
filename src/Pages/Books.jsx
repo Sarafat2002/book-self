c8371986ad9a5b5144d87155ee47b24a5cf97f51
Sarafat2002 <sarafathossen58@gmail.com>
2026-07-08 23:39:40 +0600

    ract tab added

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,15 +1,35 @@
 import React, { useContext } from 'react'
 import { BookContext } from '../ContextApi/BookProvider'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import ListedBooks from '../Components/ListedBooks';
+import WishList from '../Components/WishList';
 
 
 const Books = () => {
 
-  const {bookStore,wishlistStore} = useContext(BookContext);
-  console.log(bookStore,wishlistStore);
+  const { bookStore, wishlistStore } = useContext(BookContext);
+
 
   return (
-    <div>
-      <h1>Upcomming next lesson</h1>
+    <div className='w-9/12 mx-auto my-10'>
+
+
+      <Tabs>
+        <TabList>
+          <Tab>ReadList</Tab>
+          <Tab>WishList</Tab>
+        </TabList>
+
+        <TabPanel>
+          <ListedBooks bookStore={bookStore} />
+        </TabPanel>
+        <TabPanel>
+          <WishList wishlistStore={wishlistStore} />
+
+        </TabPanel>
+      </Tabs>
+
     </div>
   )
 }
